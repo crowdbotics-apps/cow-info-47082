aa0fb59f1b934adf719d413a31ebd10575f1b9e6
Crowdbotics <team@crowdbotics.com>
2024-03-15 11:56:12 +0000

    Initial commit

---
+++ backend/cow_info_47082/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for cow_info_47082 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "cow_info_47082.settings"
+)
+
+application = get_wsgi_application()
